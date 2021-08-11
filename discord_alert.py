1f63487a9d0c7f16eec98a27912d665596bd67f4
frgnvola <88749163+frgnvola@users.noreply.github.com>
2021-08-10 18:21:12 -0700

    removed unnecessary code

--- discord_alert.py
+++ discord_alert.py
@@ -4,9 +4,8 @@ message = ""
 
 WEBHOOK_STOCKS_URL = "https://discordapp.com/api/webhooks/870028234312523876/2Rw0-I8LKCS5pEIgXkHtx0tAKiTjEsf37iu21udcGOLWD3nzqaCFMiY2oAKnTAYqP_40"
 WEBHOOK_GENERAL_URL = "https://discordapp.com/api/webhooks/870042047795576853/mcQP_Wt3us18HgcNguLnF3-HnCqqjHRNIhGSSzA_hMx_Lipu_u_J3K0ZSk1FWSJJTnop"
-STENCHPREET_URL = "https://pbs.twimg.com/profile_images/532700911714729984/T1TYuL-S.jpeg"
-GANDALF_URL = "https://static.wixstatic.com/media/065e6b_dd1a8624bd5b40c9848aeec671ed811b.png/v1/fit/w_936%2Ch_733%2Cal_c/file.png"
-PEREGRIN_URL = "https://i.pinimg.com/originals/3b/44/c1/3b44c1b3c5be7dda5cd666d16196cd49.jpg"
+
+BOT_IMG_URL = "https://static.wixstatic.com/media/065e6b_dd1a8624bd5b40c9848aeec671ed811b.png/v1/fit/w_936%2Ch_733%2Cal_c/file.png"
 
 
 bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='5m', limit=500)
@@ -18,7 +17,6 @@ rsi = df.ta.rsi()
 df = pd.concat([df, adx, macd, rsi], axis=1)
 
 last_row = df.iloc[-1]
-print(last_row)
 
 if last_row['ADX_14'] >= 25:
     if last_row['DMP_14'] > last_row['DMN_14']:
@@ -30,9 +28,9 @@ if last_row['ADX_14'] < 25:
     message = f"NO TREND: Ethereum ADX is {last_row['ADX_14']:.2f}"
    
 payload = {
-    "username": "Gandalf",
-    "avatar_url": GANDALF_URL,
-    "content": "<@643698753743880204>, get back to the citadel!" 
+    "username": "Gandalf Bot",
+    "avatar_url": BOT_IMG_URL,
+    "content": message
 }
 requests.post(WEBHOOK_GENERAL_URL, json=payload)
 
